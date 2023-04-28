832eed2619f851bdd25f92a8d40004d289dd5ded
pavithiran <pavithransaravanan22@gmail.com>
2023-04-28 16:31:34 +0530

    Add comments

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. 
-
+// Randomly generate a computer's choice as 0->rock 1->paper 2->scissor
 function getComputerChoice() {
     return parseInt((Math.random()*10)%3);
 }
 
+// Prompt player to enter his choice and returns it
 function getPlayerChoice(roundNumber) {
     let choice = prompt(`Round ${roundNumber}: Choose Your Weapon (Rock, Paper, Scissor)`);
     if(choice != null){
@@ -16,35 +16,40 @@ function getPlayerChoice(roundNumber) {
     return 3;
 }
 
-function getObjectName(choice) {
+// Returns the weapon name as string based on the given choice
+function getWeaponName(choice) {
     if(choice == 0) return "Rock";
     if(choice == 1) return "Paper";
     if(choice == 2) return "Scissor";
 }
 
+// Global variabls to keep track of scores
 let computerScore = 0;
 let playerScore = 0;
 
+// Returns Scorecard as string
 function getScoreCard(){
     return `Player: ${playerScore} Computer: ${computerScore}`;
 }
 
+// Simulates one round of game
 function playRound(computerSelection, playerSelection) {
     let resultMessage = "";
     if(computerSelection == playerSelection){
-        resultMessage = `You both chose ${getObjectName(computerSelection)}`;
+        resultMessage = `You both chose ${getWeaponName(computerSelection)}`;
     }
     else if((playerSelection == 0 && computerSelection == 2) || (playerSelection == 1 && computerSelection == 0) || (playerSelection == 2 && computerSelection == 1)){
-        resultMessage = `${getObjectName(playerSelection)} beats ${getObjectName(computerSelection)}`;
+        resultMessage = `${getWeaponName(playerSelection)} beats ${getWeaponName(computerSelection)}`;
         playerScore++;
     }
     else {
-        resultMessage = `${getObjectName(computerSelection)} beats ${getObjectName(playerSelection)}`;
+        resultMessage = `${getWeaponName(computerSelection)} beats ${getWeaponName(playerSelection)}`;
         computerScore++;
     }
     return resultMessage;
 }
 
+// Get choice from computer and player. Play rounds. Show result at the end.
 function game(){
     for(let i = 0; i < 5; i++){
         let playerChoice = getPlayerChoice(i+1);
